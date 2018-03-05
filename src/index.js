3621a68a90bdd275c089ece2d38393521cb325d7
Jani Anttonen <jantto@utu.fi>
2018-03-05 11:56:24 +0200

    document code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const api = require("./api");
 const calc = require("./calc");
 const Stack = require("./mem");
 
+/**
+ * Timeout function that waits for a specified number of milliseconds
+ * @param {Number} time to wait in milliseconds
+ */
 function timeout(time) {
   try {
     return new Promise(res => setTimeout(() => res(), time));
@@ -11,26 +15,40 @@ function timeout(time) {
   }
 }
 
+/**
+ * Program loop, which fetches prices and volumes from all APIs and
+ * saves a new price which is calculated as a moving average
+ */
 async function loop() {
   try {
+    // Get prices from all active APIs
     let prices = await api.fetchPrices();
+
+    // Filter out failed API calls
     const failedIndices = prices.filter(
       (price, index) => price > 0 && price != undefined && price != NaN && index
     );
     prices = prices.filter((_, i) => !failedIndices.includes(i));
 
+    // Get 24h trading volumes for all active APIs
     let volumes = await api.fetchVolumes();
+
+    // Filter out failed API price call indices from volume data
     volumes = volumes.filter((_, i) => !failedIndices.includes(i));
 
+    // Sum of volumes of all exchanges
     const sumOfVolumes = calc.sum(volumes);
 
+    // Weigh each price by its exchange's volume
     const weightedPrices = prices.map((price, index) => {
       const weight = volumes[index] / sumOfVolumes;
       return price * weight;
     });
 
+    // Sum weighed prices together to reach a 100% weighed price
     const sum = calc.sum(weightedPrices);
 
+    // Push newest price to stack, taking a running average with previous prices
     if (Stack.prices["ETHUSD"] == undefined) {
       Stack.push("ETHUSD", sum);
     } else {
@@ -38,17 +56,23 @@ async function loop() {
       Stack.push("ETHUSD", runningAVG);
     }
 
+    // Print out the newest price
     console.log(Stack.prices["ETHUSD"][Stack.prices["ETHUSD"].length - 1]);
 
+    // Wait for POLL_INTERVAL
     await timeout(config.env.POLL_INTERVAL);
   } catch (e) {
     console.log(e.toString());
   }
+
+  // Start another iteration
   loop();
 }
 
+// Main program loop
 loop();
 
+// Allow CTRL+C to end program execution
 process.on("SIGINT", function() {
   process.exit();
 });
